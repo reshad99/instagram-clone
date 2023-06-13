7c643199b1a51d52473f506883a04c5c733bea36
Reshad Memmedov <resat619916@gmail.com>
2023-06-13 23:57:43 +0400

    intro screen ui

--- lib/presentation/global_screens/my_app.dart
+++ lib/presentation/global_screens/my_app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:instagram_clone/data/auth/bloc/auth/auth_bloc.dart';
 import 'package:instagram_clone/presentation/pages/auth/intro_screen.dart';
 import 'package:instagram_clone/services/router.dart';
 
